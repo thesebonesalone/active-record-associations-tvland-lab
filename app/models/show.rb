bdec3209bb64069ac8e4097ec73993176332b310
thesebonesalone <duikhead2@gmail.com>
2020-09-25 13:35:00 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+    def actors_list
+        self.actors.map {|value| value.full_name}
+    end
 end
